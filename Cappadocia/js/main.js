f4b3f8e98a2fab7e4f952adc0b97997b519e78fa
David K <sir.djdrygan@yandex.ru>
2022-06-17 15:08:30 +0300

    Landings
    Landing 1

---
+++ Cappadocia/js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+	$('.fairy-tail__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/previous-arrow.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/next-arrow.svg" alt=""></button>',
+		autoplay: true,
+		autoplaySpeed: 3500,
+		fade: true,
+		responsive: [
+			{
+				breakpoint: 601,
+				settings: {
+					arrows: false
+				}
+
+			}
+		]
+	});
+	$('.our-trip__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/previous-arrow.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/next-arrow.svg" alt=""></button>',
+		autoplay: true,
+		autoplaySpeed: 3500,
+		fade: true,
+		responsive: [
+			{
+				breakpoint: 601,
+				settings: {
+					arrows: false
+				}
+
+			}
+		]
+	});
+	$(".menu, .our-trip__body").on("click", "a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id = $(this).attr('href'),
+
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({ scrollTop: top }, 2000);
+	});
+
+
+});
