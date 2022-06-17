d290f54110870641b703b18286c8f8cab8d6b8fd
David K <sir.djdrygan@yandex.ru>
2022-06-17 15:21:11 +0300

    Landing

---
+++ Cuba/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+	$('.photo__slider').slick({
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/photo__images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/photo__images/arrow-right.svg" alt=""></button>',
+		autoplay: true,
+		autoplaySpeed: 2400,
+		speed: 850,
+	});
+
+});
